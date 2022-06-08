f31509b72e4c85e0463b4b693f9cdf3a27be573b
Emma Gerigscott <elgerig@gmail.com>
2022-06-08 13:13:36 -0700

    Reminder to self, please put script link at bottom of html...

--- script.js
+++ script.js
@@ -1,10 +1,61 @@
-const console = document.querySelector('.console');
-const numbers = document.querySelector('.number');
-const button = document.querySelector('button');
-const equal = document.querySelector('.equals');
-const clear = document.querySelector('.clear');
+let displayValue = '0';
+let firstOperand = null;
+let secondOperand = null;
+let firstOperator = null;
+let secondOperator = null;
+let result = null;
+const buttons = document.querySelectorAll('button');
 
+window.addEventListener('keydown', function(e) {
+    const key = document.querySelector(`button[data-key='${e.keyCode}']`);
+    key.click();
+});
 
+function updateDisplay() {
+    const display = document.getElementById('display');
+    display.textContent = displayValue;
+    if(displayValue.length > 9) {
+        display.textContent = displayValue.substring(0,9);
+    }
+}
+
+updateDisplay();
+
+function clickButton() {
+    for(let i=0; i<buttons.length; i++) {
+        buttons[i].addEventListener('click', function() {
+            if(buttons[i].classList.contains('number')) {
+                inputOperand(buttons[i].value);
+                updateDisplay();
+            } else if(buttons[i].classList.contains('operator')) {
+                inputOperator(buttons[i].value);
+            } else if(buttons[i].classList.contains('equals')) {
+                inputEquals();
+                updateDisplay();
+            } else if(buttons[i].classList.contains('clear'))
+            clearDisplay();
+            updateDisplay();
+        }
+   )}
+}
+
+clickButton();
+
+function inputOperand(number) {
+    if(firstOperator === null) {
+        if(displayValue === '0' || displayValue === 0) {
+            displayValue = number;
+        } else {
+            displayValue += number;
+        }
+    } else {
+        if(displayValue === firstOperand) {
+            displayValue = number;
+        } else {
+            displayValue += number;
+        }
+    }
+}
 
 function add(a,b) {
     return a + b;
@@ -28,8 +79,5 @@ function operate(operator,a,b) {
     if (operator === 'multiply') return multiply(a,b);
     if (operator === 'divide') return divide(a,b);
 }
-
-function populate()
-        
 
 
