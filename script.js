2bdf5a4d862b0ac86d125266b879a57fdb6035a0
Emma Gerigscott <elgerig@gmail.com>
2022-06-16 09:37:18 -0700

    use eventListenter and text content instead of values

--- script.js
+++ script.js
@@ -1,137 +1,91 @@
-let displayValue = '0';
-let firstNum = null;
-let secondNum = null;
-let firstOperator = null;
-let secondOperator = null;
-let result = null;
+let calcDisplay = document.querySelector('#display')
+let displayValue = '';
+let currentValue = '';
+let storedValue = 0;
+let ops = '';
 
-const buttons = document.querySelectorAll('button');
+document.querySelectorAll('.number').forEach(button => 
+    button.addEventListener('click', () => display(button.textContent)))
 
-function updateDisplay() {
-    const display = document.getElementById('display');
-    display.textContent = displayValue;
-    if(displayValue.length > 9) {
-        display.textContent = displayValue.substring(0,9);
+function display(num) {
+    if(ops === '=') {
+        storedValue = 0;
+    }
+    if(num === '0' && ops === '/' && currentValue === '') {
+        calcDisplay.textContent = 'NaN';
+        storedValue = NaN;
+    } else if(currentValue != '0') {
+        currentValue = currentValue + num;
+        calcDisplay.textContent = currentValue
     }
 }
 
-updateDisplay();
+document.querySelectorAll('.operator').forEach(button => 
+    button.addEventListener('click', () => storeOperator(button.textContent)));
 
-function clickButton() {
-    for(let i=0; i<buttons.length; i++) {
-        buttons[i].addEventListener('click', function() {
-            if(buttons[i].classList.contains('number')) {
-                inputNum(buttons[i].value);
-                updateDisplay();
-            } else if(buttons[i].classList.contains('operator')) {
-                inputOperator(buttons[i].value);
-            } else if(buttons[i].classList.contains('equals')) {
-                
-                updateDisplay();
-            } else if(buttons[i].classList.contains('clear'))
-            clearDisplay();
-            updateDisplay();
-        }
-   )}
-}
-
-clickButton();
-
-function inputNum(number) {
-    if(firstOperator === null) {
-        if(displayValue === '0' || displayValue === 0) {
-            displayValue = number;
-        } else {
-            displayValue += number;
-        }
+function storeOperator(type) {
+    if(storedValue === 0 && currentValue > 0) {
+        storedValue = parseFloat(currentValue);
+        currentValue = '';
+    } else if(storedValue === 0 && currentValue < 0) {
+        storedValue = parseFloat(currentValue);
+        currentValue = '';
+    }
+    if(ops != '') {
+        operate();
+        ops = type;
     } else {
-        if(displayValue === firstNum) {
-            displayValue = number;
-        } else {
-            displayValue += number;
-        }
+        calcDisplay.textContent = type;
+        ops = type;
+    }
+}
+document.querySelector('.equals').addEventListener('click', equals);
+
+function equals() {
+    if(ops!== '') {
+        operate();
+        ops = '=';
     }
 }
 
-function inputOperator(operator) {
-    if(firstOperator != null && secondOperator === null) {
-        secondOperator = operator;
-        secondOperand = displayValue;
-        result = operate(firstOperator, Number(firstNum), Number(secondNum));
-        firstNum = displayValue;
-        result = null;
-    } else if(firstOperator != null && secondOperator != null) {
-        secondNum = displayValue;
-        result = operate(secondOperator, Number(firstNum), Number(secondNum));
-        secondOperator = operator;
-        firstNum = displayValue;
-        result = null;
-    } else {
-        firstOperator = operator;
-        firstNum = displayValue;
-    }
-}
-/*
-function inputEquals() {
-    if(firstOperator === null) {
-        displayValue = displayValue;
-    } else if(secondOperator != null) {    
-            secondNum = displayValue; 
-            result = operate(secondOperator, Number(firstNum), Number(secondNum)); 
-            if(result === 'lmao') {
-                displayValue = 'lmao';
-        } else {
-            firstNum = displayValue;
-            secondNum = null;
-            firstOperator = null;
-            secondOperator = null;
-            result = null;
-    } 
-}else {
-        secondNum = displayValue;
-        result = operate(firstOperator, Number(firstNum), Number(secondNum));
-        if(result === 'lmao') {
-            displayValue = 'lmao';
-        } else {
-            firstNum = displayValue;
-            secondNum = null;
-            firstOperator = null;
-            secondOperator = null;
-            result = null;
-        }
-    }
-} 
-*/
-function clearDisplay() {
-    displayValue = '0';
-    firstNum = null;
-    secondNum = null;
-    firstOperator = null;
-    secondOperator = null;
-    result = null;
-}
+document.querySelector('.clear').addEventListener('click', clear);
 
+function clear() {
+    currentValue = '';
+    storedValue = 0;
+    ops = '';
+    calcDisplay.textContent = '0';
+}
+   
 function add(a,b) {
-    return a + b;
+    storedValue = parseFloat(a + b);
 }
 
 function subtract(a,b) {
-    return a - b;
+    storedValue = parseFloat(a-b);
 }
 
 function multiply(a,b) {
-    return a * b;
+    storedValue = parseFloat(a*b);
 }
 
 function divide(a,b) {
-    return a / b
+    storedValue = parseFloat(a/b)
 }
 
-function operate(operator,a,b) {
-    if (operator === 'add') return add(a,b);
-    if (operator === 'subtract') return subtract(a,b);
-    if (operator === 'multiply') return multiply(a,b);
-    if (operator === 'divide') return divide(a,b);
+function operate() {
+    if (ops === '+') {
+        result = add(storedValue, parseFloat(currentValue));
+    } else if (ops === '-') {
+        result = subtract(storedValue, parseFloat(currentValue));
+    } else if (ops === '*') {
+        result = multiply(storedValue, parseFloat(currentValue));
+    } else if (ops === '/') {
+        result = divide(storedValue, parseFloat(currentValue));
+    }
+    calcDisplay.textContent = storedValue;
+    currentValue = '';
 }
+
 
 
